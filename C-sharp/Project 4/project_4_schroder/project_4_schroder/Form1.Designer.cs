c16be941a51a8314e40154c4430d97c39abcef81
Allison Schroder <41591246+a-becks@users.noreply.github.com>
2018-10-16 11:36:17 -0700

    Add files via upload

---
+++ C-sharp/Project 4/project_4_schroder/project_4_schroder/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace project_4_schroder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtArrivalDate = new System.Windows.Forms.TextBox();
+            this.txtDepartureDate = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumNights = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPricePerNight = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTotalPrice = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Arrival date:";
+            // 
+            // txtArrivalDate
+            // 
+            this.txtArrivalDate.Location = new System.Drawing.Point(167, 39);
+            this.txtArrivalDate.Name = "txtArrivalDate";
+            this.txtArrivalDate.Size = new System.Drawing.Size(100, 22);
+            this.txtArrivalDate.TabIndex = 1;
+            // 
+            // txtDepartureDate
+            // 
+            this.txtDepartureDate.Location = new System.Drawing.Point(167, 76);
+            this.txtDepartureDate.Name = "txtDepartureDate";
+            this.txtDepartureDate.Size = new System.Drawing.Size(100, 22);
+            this.txtDepartureDate.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Departure date:";
+            // 
+            // txtNumNights
+            // 
+            this.txtNumNights.Location = new System.Drawing.Point(167, 115);
+            this.txtNumNights.Name = "txtNumNights";
+            this.txtNumNights.ReadOnly = true;
+            this.txtNumNights.Size = new System.Drawing.Size(100, 22);
+            this.txtNumNights.TabIndex = 5;
+            this.txtNumNights.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Number of nights:";
+            // 
+            // txtPricePerNight
+            // 
+            this.txtPricePerNight.Location = new System.Drawing.Point(167, 153);
+            this.txtPricePerNight.Name = "txtPricePerNight";
+            this.txtPricePerNight.ReadOnly = true;
+            this.txtPricePerNight.Size = new System.Drawing.Size(100, 22);
+            this.txtPricePerNight.TabIndex = 7;
+            this.txtPricePerNight.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Price per night:";
+            // 
+            // txtTotalPrice
+            // 
+            this.txtTotalPrice.Location = new System.Drawing.Point(167, 191);
+            this.txtTotalPrice.Name = "txtTotalPrice";
+            this.txtTotalPrice.ReadOnly = true;
+            this.txtTotalPrice.Size = new System.Drawing.Size(100, 22);
+            this.txtTotalPrice.TabIndex = 9;
+            this.txtTotalPrice.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 194);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Total price:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(38, 248);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(94, 40);
+            this.btnCalculate.TabIndex = 10;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(173, 248);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(94, 40);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 337);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtTotalPrice);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPricePerNight);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNumNights);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDepartureDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtArrivalDate);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Reservations";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtArrivalDate;
+        private System.Windows.Forms.TextBox txtDepartureDate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumNights;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPricePerNight;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTotalPrice;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ C-sharp/Project 4/project_4_schroder/project_4_schroder/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project_4_schroder
+{
+    public partial class Form1 : Form
+    {
+        DateTime currentDate = DateTime.Today;
+
+        public Form1()
+        {
+            InitializeComponent();
+            txtArrivalDate.Text = currentDate.ToShortDateString();
+            txtDepartureDate.Text = currentDate.AddDays(3).ToShortDateString();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (!isValid()) {
+                txtArrivalDate.Focus();
+                return;
+            }
+
+            const int pricePerNight = 115;
+            TimeSpan numberOfNights;
+            int totalPrice;
+
+            numberOfNights = DateTime.Parse(txtDepartureDate.Text) - DateTime.Parse(txtArrivalDate.Text);
+            totalPrice = numberOfNights.Days * pricePerNight;
+
+            txtPricePerNight.Text = pricePerNight.ToString("c");
+            txtNumNights.Text = numberOfNights.Days.ToString();
+            txtTotalPrice.Text = totalPrice.ToString("c");    
+        }
+
+        private bool isValid()
+        {
+            bool isValid = true;
+
+            //check if both are valid dates and store dates if successful
+            if (!DateTime.TryParse(txtArrivalDate.Text, out DateTime arrivalDate) || !DateTime.TryParse(txtDepartureDate.Text, out DateTime departureDate))
+            {
+                isValid = false;
+                MessageBox.Show("Please enter a valid date in the form MM/DD/YYYY.");
+            }
+
+            //make sure departure date is after arrival date
+            else if (arrivalDate > departureDate )
+            {
+                isValid = false;
+                MessageBox.Show("Departure must be after arrival date.");
+            }
+
+            // make sure reservation is for a future date
+            else if (currentDate > arrivalDate)
+            {
+                isValid = false;
+                MessageBox.Show("Please enter a date after today's date.");
+            }
+
+            //make sure it is within 5 years of today
+            else if (arrivalDate > currentDate.AddYears(5))
+            {
+                isValid = false;
+                MessageBox.Show("Reservations must be within five years of today's date.");
+            }
+            return isValid;
+        }
+
+     }
+}
