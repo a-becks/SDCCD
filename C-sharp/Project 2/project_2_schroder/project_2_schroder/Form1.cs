64df6cefea87753875dd275fa1721a82b0fd91b5
Allison Schroder <41591246+a-becks@users.noreply.github.com>
2018-10-16 11:26:57 -0700

    Add files via upload

---
+++ C-sharp/Project 2/project_2_schroder/project_2_schroder/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project_2_schroder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            int startNum = Convert.ToInt16(txtNumber.Text);
+            long factorial = calculateFactorial(startNum);
+            txtFactorial.Text = factorial.ToString("n0");
+        }
+
+        private long calculateFactorial(int num)
+        {
+            if (num > 1)
+            {
+                return num * calculateFactorial(num - 1);
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
